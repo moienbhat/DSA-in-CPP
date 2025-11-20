4e011b323565ed494586fb7fc618c7661a145140
Moien Bhat <bhatmoien34@gmail.com>
2025-11-20 18:43:26 +0530

    Add files via upload

---
+++ BalancedParenthesis.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+bool isValid(string s) {
+    stack <char>st;
+    for(auto it: s) {
+        if(it =='(' || it == '{'  || it == '[') st.push(it);
+        else {
+            if(st.size() == 0) return false;
+            char ch = st.top();
+            st.pop();
+            if((it ==')' && ch == '(') || (it == ']' && ch =='[') || (it =='}' && ch =='{')) continue;
+            else return false;
+        }
+    }
+    return st.empty();
+}
+
+int main() {
+    string s = "()[{}()]";
+    if(isValid(s))
+    cout<< "True";
+    else 
+    cout<< "false";
+    return 0;
+}
