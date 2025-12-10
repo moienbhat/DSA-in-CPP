786b8bbcb42a1a2b4ea6065d3abd5d1b11165214
Moien Bhat <bhatmoien34@gmail.com>
2025-12-10 22:38:45 +0530

    Add files via upload

---
+++ NumberOfSubstringhaving3Characters.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Solution {
+    public:
+    int numberOfSubstrings(string s) {
+        vector <int> freq(3, 0);
+        int left = 0, res = 0;;
+
+        for(int right = 0; right < s.length(); right++) {
+            freq[s[right] - 'a']++;
+
+            while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+                res += (s.length() - right);
+                freq[s[left] - 'a']--;
+                left++;
+            }
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "abcabc";
+    cout<< " The number of substrings containing all three characters are : " << sol.numberOfSubstrings(s) << endl;
+    return 0;
+}
